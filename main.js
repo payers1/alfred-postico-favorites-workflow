c8b3022be31f0b4a064798fd3fc41cfba32e5b00
Phill Ayers <phillip.ayers@sharpnotions.com>
2024-02-18 09:40:12 -0500

    Update dependencies and improve code structure

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-const alfy = require('alfy');
-const db   = require('sqlite');
-const P    = require('bluebird');
+import alfy from 'alfy';
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
 const location = process.env.HOME +
   '/Library/Containers/at.eggerapps.Postico/Data/Library/Application\ Support/' +
   'Postico/ConnectionFavorites.db';
 
-const buildConnectionString = ({ZUSER, ZHOST, ZPORT, ZDATABASE}) => [
-    'postgresql://',
-    `${ZUSER ? ZUSER + '@' : ''}`,
-    `${ZHOST || 'localhost'}`,
-    `${ZPORT ? ':' + ZPORT : ''}`, '/',
-    `${ZDATABASE || ''}`
-  ].join('');
 
-const getAllFavorites = () => db.all('SELECT * FROM ZPGEFAVORITE');
+const buildConnectionString = ({ ZUSER, ZHOST, ZPORT, ZDATABASE }) => [
+  'postgresql://',
+  `${ZUSER ? ZUSER + '@' : ''}`,
+  `${ZHOST || 'localhost'}`,
+  `${ZPORT ? ':' + ZPORT : ''}`, '/',
+  `${ZDATABASE || ''}`
+].join('');
 
-return P.resolve(db.open(location))
-  .then(getAllFavorites)
+const db = await open({
+  filename: location,
+  driver: sqlite3.Database
+})
+const favorites = await db.all('SELECT * FROM ZPGEFAVORITE');
+const filteredFavorites = favorites.filter(favorite => favorite.ZNAME || favorite.ZNICKNAME)
+
+
+const items = alfy.inputMatches(filteredFavorites, (item, input) => {
+  if (input === '') return true
+  const nicknameMatches = item.ZNICKNAME?.toLowerCase().includes(input.toLowerCase()) ?? false
+  const nameMatches = item.ZNAME?.toLowerCase().includes(input.toLowerCase()) ?? false
+  return nicknameMatches || nameMatches
+})
   .map(favorite => ({
-    title: favorite.ZNICKNAME && favorite.ZNICKNAME,
-    subtitle: buildConnectionString(favorite),
+    title: favorite.ZNICKNAME ?? favorite.ZNAME,
+    subtitle: favorite.ZHOST ?? 'localhost',
     arg: buildConnectionString(favorite)
   }))
-  .then(alfy.output);
+
+alfy.output(items)
+
+
